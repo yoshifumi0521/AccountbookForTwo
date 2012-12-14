40242374e8bede8107dea4a1ce74713ad26a32c9
YoshifumiSato <yoshifumi0521@gmail.com>
2012-12-14 20:29:54 +0900

    seedデータを追加して、Userモデルにバリデーションをつける。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
+#coding: utf-8
+#Userモデル
 class User < ActiveRecord::Base
+  
   attr_accessible :follow_id, :match, :name, :partner_id, :uid, :wait
+
+  #バリデーションをつける。
+  validates :name,:presence => true
+  validates :uid, :presence => true, :uniqueness=>true
+
+
+
 end
