bd111792561a2dd2394fcaf05390e1b0bf727be9
YoshifumiSato <yoshifumi0521@gmail.com>
2012-12-14 12:39:36 +0900

    Railsアプリを追加した

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AccountbookForTwo::Application.config.secret_token = '19cd3f083421cc8cab8e5338039e399dad66aee316322df451449e9053e3fcc93b044a2eecb9ed03509b5741191c1f1ffbbf6b402f223149831c81e588282ac9'
